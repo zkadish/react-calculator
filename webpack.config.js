465f917819d52de2bb7affa654986cc8f9addc68
zachary kadish <zachkadish@gmail.com>
2016-05-01 07:08:58 -0700

    webpack-boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+const npmInstallWebpackPlugin = require('npm-install-webpack-plugin');
+
+const TARGET = process.env.npm_lifecycle_event;
+
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
+};
+
+const common = {
+  entry: {
+    app: PATHS.app
+  },
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        include: PATHS.app
+      }
+    ]
+  }
+};
+
+if(TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devtool: 'eval-source-maps',
+    devServer: {
+      contentBase: PATHS.build,
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+      stats: 'errors-only',
+      host: process.env.HOST,
+      port: process.env.PORT
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new npmInstallWebpackPlugin({
+        save: true
+      })
+    ]
+  });
+}
+
+if(TARGET === 'build') {
+  module.exports = merge(common, {});
+}
